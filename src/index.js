cf7722276a240033bd7690f496c70aee9a32ba27
osakanara <47262417+osakanara@users.noreply.github.com>
2022-05-14 00:16:36 +0900

    完了ボタンの実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div 生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  // 完了ボタン押下時
+  completeButton.addEventListener("click", () => {
+    // 完了ボタンと削除ボタンを削除する
+    completeButton.remove();
+    deleteButton.remove();
+    // 戻すボタンを作成
+    const returnButton = document.createElement("button");
+    returnButton.innerText = "戻す";
+    div.appendChild(returnButton);
+    document.getElementById("complete-list").appendChild(div);
+  });
+
+  // 削除ボタン押下時
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親要素（div)を未完了リストから削除する
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // divタグの子要素に各要素を入れる
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
